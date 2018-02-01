8443980c9542430bceacca46edc3f6bdb5f0b28e
fabriziov <venfab@gmail.com>
2018-02-01 15:18:11 +0100

    fixing typos

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import numpy as np
-lunch_option = {"soup": {"vegetarian": True, "just_one_friday": False},
-                "habibi": {"vegetarian": True, "just_one_friday": False},
-                "vapiano": {"vegetarian": True, "just_one_friday": False},
-                "doener": {"vegetarian": True, "just_one_friday": False},
-                "asian": {"vegetarian": True, "just_one_friday": False},
-                "la_lucha": {"vegetarian": True, "just_one_friday": False},
-                "kuhbar": {"vegetarian": True, "just_one_friday": False},
-                "lostaria": {"vegetarian": True, "just_one_friday": True}}
+lunch_option = {"soup": {"vegetarian": True, "just_on_friday": False},
+                "habibi": {"vegetarian": True, "just_on_friday": False},
+                "vapiano": {"vegetarian": True, "just_on_friday": False},
+                "doener": {"vegetarian": True, "just_on_friday": False},
+                "asian": {"vegetarian": True, "just_on_friday": False},
+                "la_lucha": {"vegetarian": True, "just_on_friday": False},
+                "kuhbar": {"vegetarian": True, "just_on_friday": False},
+                "losteria": {"vegetarian": True, "just_on_friday": True}}
 
-lunch_option_vegatarian = {k: v for k, v in lunch_option.iteritems() if v["vegetarian"]}
-lunch_option_not_on_friday = {k: v for k, v in lunch_option.iteritems() if v["just_one_friday"] is False}
+lunch_option_vegetarian = {k: v for k, v in lunch_option.iteritems() if v["vegetarian"]}
+lunch_option_not_on_friday = {k: v for k, v in lunch_option.iteritems() if v["just_on_friday"] is False}
 
 random_place = np.random.choice(lunch_option.keys())
 print(random_place)
