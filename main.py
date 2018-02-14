3ce5308378c7be6b0dc13c806db33f4755295fa3
fabriziov <venfab@gmail.com>
2018-02-14 11:41:55 +0100

    adding open on monday

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import numpy as np
-lunch_option = {"soup": {"vegetarian": True, "just_on_friday": False},
-                "habibi": {"vegetarian": True, "just_on_friday": False},
-                "vapiano": {"vegetarian": True, "just_on_friday": False},
-                "doener": {"vegetarian": True, "just_on_friday": False},
-                "asian": {"vegetarian": True, "just_on_friday": False},
-                "la_lucha": {"vegetarian": True, "just_on_friday": False},
-                "kuhbar": {"vegetarian": True, "just_on_friday": False},
-                "losteria": {"vegetarian": True, "just_on_friday": True}}
+lunch_option = {"vis_a_vis": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
+                "habibi": {"vegetarian": True, "just_on_friday": False, "close_on_monday": True},
+                "vapiano": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
+                "doener": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
+                "asian": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
+                "la_lucha": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
+                "kuhbar": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
+                "l_osteria": {"vegetarian": True, "just_on_friday": True, "close_on_monday": False}}
 
 lunch_option_vegetarian = {k: v for k, v in lunch_option.iteritems() if v["vegetarian"]}
 lunch_option_not_on_friday = {k: v for k, v in lunch_option.iteritems() if v["just_on_friday"] is False}
+lunch_option_open_on_monday = {k: v for k, v in lunch_option.iteritems() if v["close_on_monday"] is False}
 
-random_place = np.random.choice(lunch_option.keys())
-print(random_place)
-
-
+print "Random place: " + np.random.choice(lunch_option.keys())
+print "Random VEG place: " + np.random.choice(lunch_option_vegetarian.keys())
+print "Random place open on Monday: ", np.random.choice(lunch_option_open_on_monday.keys())
