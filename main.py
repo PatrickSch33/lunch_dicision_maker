7fe3320e35a864bb2c4f32e3d83b0d9a99a8a96a
fabriziov <venfab@gmail.com>
2018-08-03 12:54:36 +0200

    adding new places

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ lunch_option = {"vis_a_vis": {"vegetarian": True, "just_on_friday": False, "clos
                 "vapiano": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
                 "doener": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
                 "asian": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
+                "asian_big": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
                 "la_lucha": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
                 "kuhbar": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
                 "haroun_s": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
-                "l_osteria": {"vegetarian": True, "just_on_friday": True, "close_on_monday": False}}
+                "l_osteria": {"vegetarian": True, "just_on_friday": True, "close_on_monday": False},
+                "suppkult": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False},
+                "chinese_luisenplatz": {"vegetarian": True, "just_on_friday": False, "close_on_monday": False}}
 
 lunch_option_vegetarian = {k: v for k, v in lunch_option.iteritems() if v["vegetarian"]}
 lunch_option_not_on_friday = {k: v for k, v in lunch_option.iteritems() if v["just_on_friday"] is False}
@@ -16,3 +19,5 @@ lunch_option_open_on_monday = {k: v for k, v in lunch_option.iteritems() if v["c
 print "Random place: " + np.random.choice(lunch_option.keys())
 print "Random VEG place: " + np.random.choice(lunch_option_vegetarian.keys())
 print "Random place open on Monday: ", np.random.choice(lunch_option_open_on_monday.keys())
+
+#TODO add weather condition and distance condition 
